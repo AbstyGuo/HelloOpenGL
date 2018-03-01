2340b5e1419420e7d9ab90f255575fda01644298
guoyf <guoyf@mapbar.com>
2018-03-01 16:29:42 +0800

    Initial Commit

---
+++ HelloOpenGL/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloOpenGL
+//
+//  Created by guoyf on 2018/3/1.
+//  Copyright © 2018年 guoyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloOpenGL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloOpenGL
+//
+//  Created by guoyf on 2018/3/1.
+//  Copyright © 2018年 guoyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
